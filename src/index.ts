2b5916602b0bd37b47eea8cca2c816813995b005
reza-yari <rezayari.57@gmail.com>
2022-07-29 19:44:44 +0430

    intial project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import server from './server';
+
+
+
+const port = (process.env.PORT || 3000);
+
+
+
+
+server.listen(port , () => {
+    console.log(`listening on port ${port}`)
+})
---
+++ src/server.ts
@@ -0,0 +1,32 @@
+// import cookieParser from 'cookie-parser';
+// import StatusCodes from 'http-status-codes';
+// import { Server as SocketIo } from 'socket.io';
+import express, { NextFunction, Request, Response } from 'express';
+import http from 'http';
+
+
+const app = express();
+
+
+/************************************************************************************
+ *                              Set basic express settings
+ ***********************************************************************************/
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Chat page
+app.get('/test', (req: Request, res: Response) => {
+        return res.json({
+            statusCode: 200,
+            isSuccess: true,
+            message: 'this is test api'
+        });
+    
+});
+
+
+
+const server = http.createServer(app);
+
+export default server;
